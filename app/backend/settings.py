ed266c66565a56954b45185b45b84ae08c135848
kabradshaw1 <itllbok82@gmail.com>
2023-05-04 11:42:04 -0400

    trying to deploy

--- app/backend/settings.py
+++ app/backend/settings.py
@@ -147,8 +147,13 @@ STATIC_ROOT = '/vol/web/static'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 
-
-CORS_ALLOWED_ORIGINS = os.environ.get('CORES_ALLOWED_ORIGINS', '').split(','),
+CORS_ALLOWED_ORIGINS = []
+CORS_ALLOWED_ORIGINS.extend(
+    filter(
+        None,
+        os.environ.get('CORS_ALLOWED_ORIGINS', '').split(','),
+    )
+)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
