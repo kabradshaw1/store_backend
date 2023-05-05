2e5ce31385c559376ec997e92deb92463f428072
kabradshaw1 <itllbok82@gmail.com>
2023-05-05 11:34:37 -0400

    documentation up and ready to deploy

--- app/backend/urls.py
+++ app/backend/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 from rest_framework.routers import SimpleRouter
 from user.views import UserViewSet, LoginViewSet, RefreshViewSet, RegistrationViewSet
 from store.views import ItemViewSet, OrderViewSet, CategoryViewSet, checkout
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView
+)
 
 routes = SimpleRouter()
 
@@ -18,10 +24,15 @@ routes.register(r'user', UserViewSet, basename='user')
 routes.register(r'item', ItemViewSet, basename='item')
 routes.register(r'category', CategoryViewSet, basename='category')
 routes.register(r'order', OrderViewSet, basename='order')
-# routes.register(r'checkout', CheckOutViewSet, basename='checkout') CheckOutViewSet,
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(routes.urls)),
-    path('api/checkout/', checkout, name='checkout')
-]
+    path('api/checkout/', checkout, name='checkout'),
+    path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
+    path(
+        'api/docs/',
+        SpectacularSwaggerView.as_view(url_name='api-schema'),
+        name='api-docs',
+    ),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
