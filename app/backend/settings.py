ec8d5d2f8b7d315063d8b9fd19f7a04cd40a0781
kabradshaw1 <itllbok82@gmail.com>
2023-05-07 13:58:53 -0400

    trying to deploy

--- app/backend/settings.py
+++ app/backend/settings.py
@@ -52,14 +52,14 @@ INSTALLED_APPS = [
     'user',
 
     'rest_framework',
-    'corsheaders',
+    # 'corsheaders',
     'drf_spectacular',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    # 'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
