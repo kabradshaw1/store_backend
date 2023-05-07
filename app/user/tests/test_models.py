d4a44bf80c6c51816827b47cc5d6cdfa49be0538
kabradshaw1 <itllbok82@gmail.com>
2023-05-07 11:28:03 -0400

    done with tests for now

--- app/user/tests/test_models.py
+++ app/user/tests/test_models.py
@@ -1,3 +1,23 @@
 """
 Tests for user models.
 """
+from django.test import TestCase
+
+from user.models import User
+
+class ModelTests(TestCase):
+  """Test user models."""
+
+  def test_create_user_with_email_successful(self):
+    """Test creating a user with an email is successful."""
+    email = 'test@example.com'
+    password = 'testpass123'
+    user = User.objects.create_user(
+      username=email,
+      email=email,
+      password=password,
+    )
+
+    self.assertEqual(user.email, email)
+    self.assertTrue(user.check_password(password))
+
