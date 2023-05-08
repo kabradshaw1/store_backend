0e33a869dc8094ab26e6b012144437eb975b0d60
kabradshaw1 <itllbok82@gmail.com>
2023-05-08 14:27:11 -0400

    set cors origins

--- app/backend/settings.py
+++ app/backend/settings.py
@@ -148,14 +148,14 @@ STATIC_ROOT = '/vol/web/static'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 
-# CORS_ALLOWED_ORIGINS = []
-# CORS_ALLOWED_ORIGINS.extend(
-#     filter(
-#         None,
-#         os.environ.get('CORS_ALLOWED_ORIGINS', '').split(','),
-#     )
-# )
-CORS_ALLOW_ALL_ORIGINS: True
+CORS_ALLOWED_ORIGINS = []
+CORS_ALLOWED_ORIGINS.extend(
+    filter(
+        None,
+        os.environ.get('CORS_ALLOWED_ORIGINS', '').split(','),
+    )
+)
+# CORS_ALLOW_ALL_ORIGINS: True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
